872af2a5a381bb230889180c22bb19080176ebbd
Bram Adams <3282661+bramses@users.noreply.github.com>
2023-10-16 09:09:24 -0400

    life-of-mind

---
+++ javascript/life-of-mind/poem.js
@@ -0,0 +1,26 @@
+/**
+ * the throne denies its ruler
+ * of getting up and walking away
+ * forced to observe the kingdom that he rules
+ * without ever truly being a part of it
+ * 
+ * the throne itself touches the ground
+ * so the ruler thinks he does too
+ * but when the throne is removed
+ * the ruler goes with it
+ */
+
+
+/**
+ * in a stodged room, six feet above the others
+ * i rest my elbow upon a gilded throne
+ * from here i command everything that happens, there is nothing I cant know
+ * 
+ * servants bustle about, doing my bidding
+ * but when i ask to step off my throne, they conjure up excuses
+ * 
+ * because the clawed feet of my throne touch the ground
+ * i think i do too
+ * but when the throne is removed
+ * i think i go too
+ */
